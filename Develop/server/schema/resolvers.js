bfa07c62abff46e57b658f814d121b31af770e39
Haseebz1 <haseebkandore@hotmail.com>
2023-09-04 10:21:18 +0100

    mutation in resolver added

--- Develop/server/schema/resolvers.js
+++ Develop/server/schema/resolvers.js
@@ -3,6 +3,15 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
 
+    Mutation: {
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+    },  
+
     Query: {
         me: async (parent, args, context) => {
           if (context.user) {
