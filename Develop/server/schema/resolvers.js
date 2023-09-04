03c7625cadb251a807268939164a8bd747beb80f
Haseebz1 <haseebkandore@hotmail.com>
2023-09-04 10:22:53 +0100

    login mutation added

--- Develop/server/schema/resolvers.js
+++ Develop/server/schema/resolvers.js
@@ -4,6 +4,24 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 const resolvers = {
 
     Mutation: {
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+          },
+
         addUser: async (parent, args) => {
           const user = await User.create(args);
           const token = signToken(user);
