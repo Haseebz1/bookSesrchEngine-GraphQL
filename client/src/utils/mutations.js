92693dffd1bfd2c69afad62b07fc3498f889d631
Haseebz1 <haseebkandore@hotmail.com>
2023-09-04 10:42:44 +0100

    Client side mutations saving books added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,23 @@
 import { gql } from '@apollo/client';
 
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
