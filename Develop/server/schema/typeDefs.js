21b82157b00d6900e486b266fb5ae1630a793276
Haseebz1 <haseebkandore@hotmail.com>
2023-09-04 10:12:25 +0100

    Created Mutation

--- Develop/server/schema/typeDefs.js
+++ Develop/server/schema/typeDefs.js
@@ -28,9 +28,15 @@ const typeDefs = `
     title: String!
   }
 
-
   type Query {
     me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
   `;
 
