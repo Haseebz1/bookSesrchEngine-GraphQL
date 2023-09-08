d7beb775a22efaeefb3df83856a0d1eda5f683c7
Haseebz1 <haseebkandore@hotmail.com>
2023-09-08 23:49:01 +0100

    LOGIN USER mutation import and used in SignUp form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,14 +2,14 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [loginUserMutation, {error}] = useMutation(LOGIN_USER);
+  const [loginUserMutation ] = useMutation(LOGIN_USER);
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
   
